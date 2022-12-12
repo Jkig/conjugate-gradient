e23a1e7b1cb4d4eb43c84f00fa692e30dccce0c1
amwi5704 <77754630+amwi5704@users.noreply.github.com>
2022-12-12 01:56:20 -0700

    illconditioning stuffs

--- ill-cond.py
+++ ill-cond.py
@@ -11,7 +11,7 @@ from scipy.io import mmread
 
 def driver(A,b,x0):
     #Set exit parameters
-    Nmax = 100
+    Nmax = 1000
     tol = 1.0e-8
     #initial condition
     #run evaluator
@@ -23,12 +23,15 @@ def driver(A,b,x0):
     # list of abs errors vs last one
     abs_err = []
     last_index = len(xLst) - 1
-    
+
     for i in range(len(xLst)):
         error = np.linalg.norm(xLst[i]-xLst[last_index])
         abs_err.append(error)
-    
+
     plt.plot(range(0,len(xLst)),abs_err)
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Absolute Error")
+    plt.title("Number of Iterations vs Absolute Error")
     plt.show()
 
 
@@ -39,7 +42,7 @@ if __name__ == '__main__':
     b = np.array([-0.5, 1, 1])
     x0 = np.array([0,0,0])
     driver(A,b,x0)
-    
+
     # case 0 - det = 0
     # Define System of Equations
     A = np.array([[9,3],[3,1]])
@@ -50,12 +53,12 @@ if __name__ == '__main__':
     print("test case, basic fine matrix")
     driver(A, b, x0)
 
-    
+
     # case 1 - not fully positive definite (first sub determinets = 0)
     A = np.array([[4,2,1,1],[2,-1,1,1],[1,1,1,2],[1,1,2,-1]])
     b = np.array([-0.5, 1, 1,4])
     x0 = np.array([0,0,0,0])
-    print("case 1")
+    print("case 1, condition number:", np.linalg.cond(A), "results:")
     driver(A,b,x0)
 
     # case 2 - ill conditioned number 1, k = 5
@@ -63,16 +66,16 @@ if __name__ == '__main__':
     b = np.array([-0.5, 1, 1,4, 6])
     x0 = np.array([0,0,0,0,0])
     print()
-    print("case 2:", np.linalg.cond(A), "results:")
+    print("case 2, condition number:", np.linalg.cond(A), "results:")
     driver(A,b,x0)
-    
+
 
     # case 3 - ill conditioned number 2, k = 10
     A = np.array([[400000000,2,1,1],[2,-1,1,1],[1,1,1,2],[1,1,2,-1]])
     b = np.array([-0.5, 1, 1,4])
     x0 = np.array([0,0,0,0])
     print()
-    print("case 3:", np.linalg.cond(A), "size of matrix: ", len(A))
+    print("case 3, condition number:", np.linalg.cond(A), "size of matrix: ", len(A))
     driver(A,b,x0)
 
 
@@ -85,12 +88,12 @@ if __name__ == '__main__':
 
     b = np.random.rand(14)
     x0 = np.zeros(14)
-    
+
     print()
     print("case 4, condition number:", np.linalg.cond(A), "size of matrix: ", len(A))
     driver(A, b, x0)
 
-    
+
     # case 5 - not perfect, but converges in 30, 31, or 32 depending on the trial
     f = open("pref.2.matrix5.mtx", "r", encoding="utf-8")
     text = f.read()
@@ -100,11 +103,11 @@ if __name__ == '__main__':
 
     b = np.random.rand(24)
     x0 = np.zeros(24)
-    
+
     print()
     print("case 5, condition number:", np.linalg.cond(A), "size of matrix: ", len(A))
     driver(A, b, x0)
-    
+
     # case 6, little better conditioned than matrix 4, and converges a little quicker.
     f = open("pref.2.matrix6.mtx", "r", encoding="utf-8")
     text = f.read()
@@ -114,7 +117,7 @@ if __name__ == '__main__':
 
     b = np.random.rand(14)
     x0 = np.zeros(14)
-    
+
     print()
     print("case 6, condition number:", np.linalg.cond(A), "size of matrix: ", len(A))
     driver(A, b, x0)
@@ -128,8 +131,7 @@ if __name__ == '__main__':
 
     b = np.random.rand(30)
     x0 = np.zeros(30)
-    
+
     print()
     print("case 7, condition number:", np.linalg.cond(A), "size of matrix: ", len(A))
     driver(A, b, x0)
-
--- paralTest.py
+++
@@ -1,4 +0,0 @@
-
-VecCreateMPI(MPI_Comm comm,n,N,&x);
-VecCreateMPI(MPI_Comm comm,m,M,&y);
-MatMult(Mat A,Vec x, Vec y);
